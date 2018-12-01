e30dc5cda2651c63dc21640d74bc5d1026855dfb
Jay Elbourne <jayelbourne@gmail.com>
2018-12-01 15:34:50 -0500

    Got Lava Working Better while rising up.

--- Assets/Scripts/Entity/LavaController.cs
+++ Assets/Scripts/Entity/LavaController.cs
@@ -4,10 +4,56 @@ using UnityEngine;
 
 public class LavaController : MonoBehaviour {
 
-    public float raiseSpeed = 0.1f;
-	
-	// Update is called once per frame
-	void Update () {
-        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + (raiseSpeed * Time.deltaTime), transform.localPosition.z);
-	}
+    public Transform startMarker;
+    public Transform endMarker;
+
+    // Movement speed in units/sec.
+    public float speed = 1.0F;
+
+    // Time when the movement started.
+    private float startTime;
+
+    // Total distance between the markers.
+    private float journeyLength;
+
+    public LayerMask enemyLayer;
+
+    private void Awake()
+    {
+        startTime = Time.time;
+
+        // Calculate the journey length.
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // Distance moved = time * speed.
+        float distCovered = (Time.time - startTime) * speed;
+
+        // Fraction of journey completed = current distance divided by total distance.
+        float fracJourney = distCovered / journeyLength;
+
+        // Set our position as a fraction of the distance between the markers.
+        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if ((enemyLayer & 1 << other.gameObject.layer) == 1 << other.gameObject.layer)
+        {
+            Debug.Log("Lava Going Down");
+            TraitDeath td = other.gameObject.GetComponent<TraitDeath>();
+
+            //Destroy(other);
+            if (td)
+            {
+                Debug.Log("Found Enemy Death");
+                td.IsDead();
+            }
+            other.enabled = false;
+            startTime = Time.time;
+            //transform.position += Vector3.down * 5f;
+        }
+    }
 }
--- Assets/Scripts/Entity/Traits/TraitDeath.cs
+++ Assets/Scripts/Entity/Traits/TraitDeath.cs
@@ -6,9 +6,14 @@ public class TraitDeath : MonoBehaviour {
 
     public void IsDead()
     {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr) sr.enabled = false;
+
         //TODO
         //Play Sound
         //Play Animation
         //Play Particle Effects
+        Debug.Log("About to destroy");
+        Destroy(gameObject);
     }
 }
